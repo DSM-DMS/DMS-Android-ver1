e4fd39fd602eaa4ae517ce0fea43132a0b3444ca
half-but <qudcks9191@naver.com>
2017-11-26 20:10:54 +0900

    add button shape, meal fragment ~ing

--- app/src/main/java/teamdms/dms_kotlin/Fragment/MealFragment.kt
+++ app/src/main/java/teamdms/dms_kotlin/Fragment/MealFragment.kt
@@ -5,14 +5,25 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_meal.view.*
+import teamdms.dms_kotlin.R
 
 /**
  * Created by root1 on 2017. 11. 25..
  */
 class MealFragment: Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(R.layout.fragment_meal, null)
+        return view
+    }
 
+    private fun init(view: View){
+        with(view){
+            viewPager.pageMargin = 16
+            viewPager.offscreenPageLimit = 3
+
+        }
     }
 
 }
---
+++ app/src/main/java/teamdms/dms_kotlin/ViewPagerAdapter/MealViewPagerAdapter.kt
@@ -0,0 +1,19 @@
+package teamdms.dms_kotlin.ViewPagerAdapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+/**
+ * Created by root1 on 2017. 11. 26..
+ */
+class MealViewPagerAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
