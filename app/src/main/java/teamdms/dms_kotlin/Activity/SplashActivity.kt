068cee1d8f9c1eed9aa55fd611ca081e87bf51b9
half-but <qudcks9191@naver.com>
2017-11-29 22:36:30 +0900

    temp

--- app/src/main/java/teamdms/dms_kotlin/Activity/SplashActivity.kt
+++ app/src/main/java/teamdms/dms_kotlin/Activity/SplashActivity.kt
@@ -1,8 +1,8 @@
 package teamdms.dms_kotlin.Activity
 
-import android.content.*
 import android.os.*
 import team_dms.dms.Base.*
+import teamdms.dms_kotlin.*
 
 /**
  * Created by root1 on 2017. 11. 28..
@@ -11,9 +11,10 @@ class SplashActivity: BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.view_bottom_sheet)
+//        val intent = Intent(this, MainActivity::class.java)
+//        startActivity(intent)
+//        finish()
     }
 
 }
