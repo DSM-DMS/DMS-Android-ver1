7a7a2c7f14e02548b0e8f13431cd447ff0426048
yjh5424 <yjh54240@naver.com>
2017-12-26 13:30:24 +0900

    widget 틀 추가

---
+++ app/src/main/java/teamdms/dms_kotlin/Widget/MealWidget.kt
@@ -0,0 +1,45 @@
+package teamdms.dms_kotlin.Widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+import teamdms.dms_kotlin.R
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class MealWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
+                                     appWidgetId: Int) {
+
+            val widgetText = context.getString(R.string.appwidget_text)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+            views.setTextViewText(R.id.appwidget_text, widgetText)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
